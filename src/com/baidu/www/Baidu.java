1f131a00bfc2e6cf82d7810b1c8858f2df419d55
陈书飞Git <1019773507@qq.com>
2018-03-02 14:15:44 +0800

    Initial commit

---
+++ src/com/baidu/www/Baidu.java
@@ -0,0 +1,8 @@
+package com.baidu.www;
+
+/**
+ * Created by Administrator on 2018/3/2.
+ */
+public class Baidu {
+
+}
---
+++ src/helloWord/HelloWord.java
@@ -0,0 +1,10 @@
+package helloWord;
+
+/**
+ * Created by Administrator on 2018/3/1.
+ */
+public class HelloWord {
+        public static void main(String[] args) {
+            System.out.println("你好");
+        }
+}
